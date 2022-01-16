2db06fd970c3e6e85d3136c71dee0abddbceb9a5
MaryanaDey <81640002+MaryanaDey@users.noreply.github.com>
2022-01-16 21:37:20 +0200

    add modal

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -1,37 +1,18 @@
-import { Component } from 'react';
-import { createPortal } from 'react-dom';
-import PropTypes from 'prop-types';
-import { Overlay, ModalContent } from './Modal.styled';
+import styled from 'styled-components';
 
-const modalRoot = document.querySelector('#modal-root');
-
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-  handleOverlayClick = e => {
-    if (e.currentTarget === e.target) {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    return createPortal(
-      <Overlay onClick={this.handleOverlayClick}>
-        <ModalContent>{this.props.children}</ModalContent>
-      </Overlay>,
-      modalRoot,
-    );
-  }
-}
-Modal.propTypes = {
-  onClick: PropTypes.func,
-};
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 1200;
+`;
+export const ModalContent = styled.div`
+  max-width: calc(100vw - 48px);
+  max-height: calc(100vh - 24px);
+`;
